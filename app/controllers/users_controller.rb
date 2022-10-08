3ff184c836f27fafc99b61ef62f369e54c04c475
ejdiener <64671864+ejdiener@users.noreply.github.com>
2022-10-08 14:23:54 -0400

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  def new; end
-  
+  def index
+  end
   def show
     @user = User.find(params[:id])
   end
@@ -10,12 +10,20 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])    # Not the final implementation!
+    @user = User.new(user_params)
     if @user.save
-      # Handle a successful save.
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 
 end
