d846438a24cf4877fa53f29b763893dff66c888d
ejdiener <64671864+ejdiener@users.noreply.github.com>
2022-11-02 18:05:55 -0400

    fixed debug gem problems

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,13 +1,25 @@
 require_relative '../test_helper'
 
-class UsersLogin < ActionDispatch::IntegrationTest
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
   def setup
     @user = users(:michael)
   end
-end
 
-class UsersLoginTest < UsersLogin
+  test "login with valid email/invalid password" do
+    get login_path
+    assert_template 'sessions/new'
+    post login_path, params: { session: { email:    @user.email,
+                                          password: "invalid" } }
+    assert_not is_logged_in?
+    assert_template 'sessions/new'
+    assert_not flash.empty?
+    get root_path
+    assert flash.empty?
+  end
+
   test "login with valid information followed by logout" do
+    get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
     assert is_logged_in?
@@ -18,7 +30,6 @@ class UsersLoginTest < UsersLogin
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
     delete logout_path
-    assert_response :see_other
     assert_not is_logged_in?
     assert_redirected_to root_url
     # Simulate a user clicking logout in a second window.
@@ -28,81 +39,10 @@ class UsersLoginTest < UsersLogin
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
-end
-
-class InvalidPasswordTest < UsersLogin
-  test 'login path' do
-    get login_path
-    assert_template 'sessions/new'
-  end
-
-  test 'login with valid email/invalid password' do
-    post login_path, params: { session: { email: @user.email,
-                                          password: 'invalid' } }
-    assert_not is_logged_in?
-    assert_template 'sessions/new'
-    assert_not flash.empty?
-    get root_path
-    assert flash.empty?
-  end
-end
-
-class ValidLogin < UsersLogin
-  def setup
-    super
-    post login_path, params: { session: { email: @user.email,
-                                          password: 'password' } }
-  end
-end
-
-class ValidLoginTest < ValidLogin
-  test 'valid login' do
-    assert is_logged_in?
-    assert_redirected_to @user
-  end
-
-  # test 'redirect after login' do
-  #   follow_redirect!
-  #   assert_template 'users/show'
-  #   assert_select 'a[href=?]', login_path, count: 0
-  #   assert_select 'a[href=?]', logout_path
-  #   assert_select 'a[href=?]', user_path(@user)
-  # end
-
-end
-
-class Logout < ValidLogin
-  def setup
-    super
-    delete logout_path
-  end
-end
-
-class LogoutTest < Logout
-  test 'successful logout' do
-    assert_not is_logged_in?
-    assert_response :see_other
-    assert_redirected_to root_url
-  end
-
-  test 'redirect after logout' do
-    follow_redirect!
-    assert_select 'a[href=?]', login_path
-    assert_select 'a[href=?]', logout_path,      count: 0
-    assert_select 'a[href=?]', user_path(@user), count: 0
-  end
-
-  test "should still work after logout in second window" do
-    delete logout_path
-    assert_redirected_to root_url
-  end
-end
-
-class RememberingTest < UsersLogin
 
   test "login with remembering" do
     log_in_as(@user, remember_me: '1')
-    assert_not cookies[:remember_token].blank?
+    assert_not_empty cookies[:remember_token]
   end
 
   test "login without remembering" do
@@ -110,6 +50,6 @@ class RememberingTest < UsersLogin
     log_in_as(@user, remember_me: '1')
     # Log in again and verify that the cookie is deleted.
     log_in_as(@user, remember_me: '0')
-    assert cookies[:remember_token].blank?
+    assert_empty cookies[:remember_token]
   end
 end
