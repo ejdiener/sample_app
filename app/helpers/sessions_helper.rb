8eb07ecefea98b1c681b66f0a7b9c675c157a808
ejdiener <64671864+ejdiener@users.noreply.github.com>
2022-11-02 17:16:33 -0400

    9.3.2 seems to be working

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,7 +11,7 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-  # Returns the current logged-in user (if any).
+  # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
